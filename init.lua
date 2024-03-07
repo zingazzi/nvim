5567bf1f6614505d403112fc78f71d38552d6a58
Marco Zingoni <marco.zingoni@digitalvirgo.com>
2024-03-07 17:05:24 +0100

    new base config

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -15,18 +10,11 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = { "nvim-lua/plenary.nvim" }}
-}
-local opts = {}
-require("lazy").setup(plugins, opts)
+require("vim-options")
+require("lazy").setup("plugins")
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {}) 
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+
